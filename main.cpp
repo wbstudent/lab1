cf9e19a5b4edb771752b4ad59580cfd3980090a1
Wiktor Bukowski <226094@student.pwr.wroc.pl>
2016-03-23 00:20:41 +0100

    bez sprawdzania, czy NULL

--- main.cpp
+++ main.cpp
@@ -5,12 +5,11 @@
 char **charSquare(int n);
 void drawCharSquare(char **square, int n);
 
-bool allocating_error = 0;//0, gdy wszystko przebiegło w porządku, 1, gdy wystąpił błąd przy tworzeniu kwadratu
-const int max_side_length = 75;
+const int MAX_SIDE_LENGTH = 75;
 
 int main(int argc, char **argv)
 {
-	int n = -1;
+
 	if( !(argc == 2 || argc == 3) )
 	{
 		printf("Nieodpowiednia liczba argumentów!\nUżycie: ./lab1 długość_boku [seed_value]");
@@ -22,39 +21,30 @@ int main(int argc, char **argv)
 	else
 		srand(time(NULL));
 
+	int n = -1;
 	n = atoi(argv[1]);
-	if(n < 1 || n > max_side_length + 1)
+	if(n < 1 || n > MAX_SIDE_LENGTH + 1)
 	{
-		printf("Długość boku musi być zawarta w przedziale <1, %d>", max_side_length);
+		printf("Długość boku musi być zawarta w przedziale <1, %d>", MAX_SIDE_LENGTH);
 		return 2;
 	}
 
 	drawCharSquare(charSquare(n), n);
-
-	if(allocating_error == 0)
-		return 0;
-	return 3;
 }	
 
 char **charSquare(int n)
 {
 	char **square;
 	square = new char*[n];
-	if(square == NULL)
-		return NULL;
 
-	//alokowanie pamięci na kwadrat
+	//Tworzenie
 	for(int i = 0; i < n; i++)
-	{
 		square[i] = new char[n];
-		if(square[i] == NULL)
-			return NULL;
-	}
 
-	//wypełnienie kwadratu
+	//Wypełnienie kwadratu
 	for(int i = 0; i < n*n; i++)
 	{
-		square[i/n][i%n] = (char)('a' + rand()%('z'-'a' + 1));
+		square[i/n][i%n] = (char)('a' + rand()%('z'-'a' + 1) );
 	}
 
 	return square;
@@ -62,17 +52,12 @@ char **charSquare(int n)
 
 void drawCharSquare(char **square, int n)
 {	
-	if(square == NULL)
-	{
-		printf("Nastąpił błąd przy tworzeniu kwadratu.\n");
-		allocating_error = 1;
-		return;
-	}
 	for(int i = 0; i < n*n; i++)
 	{
 		printf("%c%c", square[i/n][i%n], (i%n == n-1) ? '\n' : ' ');
 	}
 
+	//Usuwanie kwadratu
 	for(int i = 0; i < n; i++)
 		delete[] square[i];
 	delete[] square;	
